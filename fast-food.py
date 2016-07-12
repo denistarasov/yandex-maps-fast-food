b8caf4cc927e553e8d8d564d13cd560ec8b7dc43
Denis Tarasov <dvtarasov@edu.hse.ru>
2016-07-12 21:19:17 +0300

    Fixed major bugs
    Now user can find the closest restaurant of a fixed brand

--- fast-food.py
+++ fast-food.py
@@ -2,6 +2,8 @@ import json
 import requests
 
 TOKEN_ID = 'bec2016b-638e-4df8-96e3-4434b5c7e05b'
+# 37.532889,55.800062 nearby Aeroport metro station
+# 37.574452,55.761205 nearby Moscow Zoo
 CENTRAL_POINT = '37.532889,55.800062' # nearby Aeroport metro station
 SEARCH_AREA_SIZE = '0.017,0.017'
 RESTAURANT_NAME = 'KFC'
@@ -10,31 +12,44 @@ class Restaurant:
     name = '' # KFC, BK or MD
     address = '' # e.g. Ленингрдаский проспект, 1
     coordinates = '' # e.g. 37.532889,55.800062
+    def __init__(self, name, address, coordinates):
+        self.name = name
+        self.address = address
+        self.coordinates = coordinates
 
-# add other restaurants except KFC (e.g. BK & McDonalds)
+# using Yandex Maps Organizations Search API
 def getData():
-    req = requests.get('https://search-maps.yandex.ru/v1/?text={}&ll={}&spn={}&type=biz&lang=ru_RU&apikey={}'.format(RESTAURANT_NAME, CENTRAL_POINT, SEARCH_AREA_SIZE, TOKEN_ID))
+    url = 'https://search-maps.yandex.ru/v1/?results=9999&text={}&ll={}&spn={}&type=biz&lang=ru_RU&apikey={}'.format(RESTAURANT_NAME, CENTRAL_POINT, SEARCH_AREA_SIZE, TOKEN_ID)
+    req = requests.get(url)
     d = json.loads(req.text) # json to dict
     number_of_search_results = d['properties']['ResponseMetaData']['SearchResponse']['found']
     restaurants = []
-    for i in range(0, number_of_search_results):
-        rest = Restaurant()
-        rest.name = d['features'][i]['properties']['CompanyMetaData']['Chains'][0]['name']
-        rest.address = d['features'][i]['properties']['CompanyMetaData']['address']
-        rest.coordinates = d['features'][i]['geometry']['coordinates']
-        restaurants.append(rest)
-    return restaurants 
+
+    i = 0
+    while True:
+        try:
+            name = d['features'][i]['properties']['CompanyMetaData']['name']
+            address = d['features'][i]['properties']['CompanyMetaData']['address']
+            coordinates_list = d['features'][i]['geometry']['coordinates']
+            coordinates = str(coordinates_list[0]) + ',' + str(coordinates_list[1])
+            restaurants.append(Restaurant(name, address, coordinates))
+            i += 1
+        except IndexError:
+            break
+        except KeyError:
+            i += 1
+            continue
+    return restaurants
 
 # using Yandex Maps Static API
 def constructMapURL(restaurants):
     s = ''
     for r in restaurants:
-        s += str(r.coordinates[0]) + ',' + str(r.coordinates[1]) + ',round~'
+        s += r.coordinates + ',round~'
     s = s[:-1] # delete last '~'
-    return 'https://static-maps.yandex.ru/1.x/?l=map&pt={}'.format(s)
+    if s == '':
+        return 'Sorry, nothing was found'
+    return 'https://static-maps.yandex.ru/1.x/?l=map&z=14&ll={}&pt={}'.format(CENTRAL_POINT, s)
 
 restaurants = getData()
-# little debugging
-# for i in restaurants:
-#    print(i.name + ', ' + i.address + ', ' + str(i.coordinates[0]) + str(i.coordinates[1]) + '\n')
 print(constructMapURL(restaurants))
