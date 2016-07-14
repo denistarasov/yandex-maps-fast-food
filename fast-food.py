a2e57f9c795f601025a5573c5015505b8121ea91
Denis Tarasov <dvtarasov@edu.hse.ru>
2016-07-14 16:34:54 +0300

    Point is now set by user
    Now user can convert his address to coordinates and set it as a central
    point

--- fast-food.py
+++ fast-food.py
@@ -4,7 +4,7 @@ import requests
 TOKEN_ID = 'bec2016b-638e-4df8-96e3-4434b5c7e05b'
 # 37.532889,55.800062 nearby Aeroport metro station
 # 37.574452,55.761205 nearby Moscow Zoo
-CENTRAL_POINT = '37.532889,55.800062' # nearby Aeroport metro station
+CENTRAL_POINT = '37.620393,55.753960' # Moscow central point
 SEARCH_AREA_SIZE = '0.017,0.017'
 RESTAURANT_NAME = 'KFC'
 
@@ -17,6 +17,14 @@ class Restaurant:
         self.address = address
         self.coordinates = coordinates
 
+# using Yandex Maps Geocoder API
+def addressToPoint(address):
+    url = 'https://geocode-maps.yandex.ru/1.x/?format=json&geocode=Москва,+{}'.format(address)
+    req = requests.get(url)
+    d = json.loads(req.text)
+    coordinates = d['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['Point']['pos'] # e.g. 37.611347 55.760241
+    return coordinates.replace(" ", ",") # '37.611347 55.760241' -> '37.611347,55.760241'
+        
 # using Yandex Maps Organizations Search API
 def getData():
     url = 'https://search-maps.yandex.ru/v1/?results=9999&text={}&ll={}&spn={}&type=biz&lang=ru_RU&apikey={}'.format(RESTAURANT_NAME, CENTRAL_POINT, SEARCH_AREA_SIZE, TOKEN_ID)
@@ -44,12 +52,19 @@ def getData():
 # using Yandex Maps Static API
 def constructMapURL(restaurants):
     s = ''
+    count = 0
     for r in restaurants:
         s += r.coordinates + ',round~'
+        count += 1
+        if count > 30:
+            break
     s = s[:-1] # delete last '~'
     if s == '':
         return 'Sorry, nothing was found'
     return 'https://static-maps.yandex.ru/1.x/?l=map&z=14&ll={}&pt={}'.format(CENTRAL_POINT, s)
 
+####################
+
+CENTRAL_POINT = addressToPoint('кочновский проезд 3')
 restaurants = getData()
 print(constructMapURL(restaurants))
