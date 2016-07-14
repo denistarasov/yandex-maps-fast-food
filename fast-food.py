3c49a1ddd705b8499820f140b50f46beea8fbf34
Denis Tarasov <dvtarasov@edu.hse.ru>
2016-07-14 17:42:20 +0300

    Find the closest restaurants of 3 types
    Now user can enter his address and get link to a map where the nearest
    restaurants of most famous brand names would be shown

--- fast-food.py
+++ fast-food.py
@@ -2,11 +2,8 @@ import json
 import requests
 
 TOKEN_ID = 'bec2016b-638e-4df8-96e3-4434b5c7e05b'
-# 37.532889,55.800062 nearby Aeroport metro station
-# 37.574452,55.761205 nearby Moscow Zoo
 CENTRAL_POINT = '37.620393,55.753960' # Moscow central point
 SEARCH_AREA_SIZE = '0.017,0.017'
-RESTAURANT_NAME = 'KFC'
 
 class Restaurant:
     name = '' # KFC, BK or MD
@@ -26,17 +23,17 @@ def addressToPoint(address):
     return coordinates.replace(" ", ",") # '37.611347 55.760241' -> '37.611347,55.760241'
         
 # using Yandex Maps Organizations Search API
-def getData():
-    url = 'https://search-maps.yandex.ru/v1/?results=9999&text={}&ll={}&spn={}&type=biz&lang=ru_RU&apikey={}'.format(RESTAURANT_NAME, CENTRAL_POINT, SEARCH_AREA_SIZE, TOKEN_ID)
+def getData(brand_name):
+    url = 'https://search-maps.yandex.ru/v1/?results=9999&text={}&ll={}&spn={}&type=biz&lang=ru_RU&apikey={}'.format(brand_name, CENTRAL_POINT, SEARCH_AREA_SIZE, TOKEN_ID)
     req = requests.get(url)
     d = json.loads(req.text) # json to dict
     number_of_search_results = d['properties']['ResponseMetaData']['SearchResponse']['found']
-    restaurants = []
+    global restaurants
 
     i = 0
-    while True:
+    while i < 10:
         try:
-            name = d['features'][i]['properties']['CompanyMetaData']['name']
+            name = brand_name # d['features'][i]['properties']['CompanyMetaData']['name']
             address = d['features'][i]['properties']['CompanyMetaData']['address']
             coordinates_list = d['features'][i]['geometry']['coordinates']
             coordinates = str(coordinates_list[0]) + ',' + str(coordinates_list[1])
@@ -47,17 +44,21 @@ def getData():
         except KeyError:
             i += 1
             continue
-    return restaurants
 
 # using Yandex Maps Static API
 def constructMapURL(restaurants):
     s = ''
-    count = 0
     for r in restaurants:
-        s += r.coordinates + ',round~'
-        count += 1
-        if count > 30:
-            break
+        global brand_names
+        if r.name == brand_names[0]:
+            point_color = 'yw'
+        elif r.name == brand_names[1]:
+            point_color = 'rd'
+        elif r.name == brand_names[2]:
+            point_color = '2rd'
+        else:
+            point_color = 'gr'
+        s += '{},pm{}m~'.format(r.coordinates, point_color)
     s = s[:-1] # delete last '~'
     if s == '':
         return 'Sorry, nothing was found'
@@ -65,6 +66,11 @@ def constructMapURL(restaurants):
 
 ####################
 
-CENTRAL_POINT = addressToPoint('кочновский проезд 3')
-restaurants = getData()
+print('Введите свое местоположение. Пример: "Красная площадь". Если адрес некорректен, то точкой отсчета будет выбран пример')
+CENTRAL_POINT = addressToPoint(input())
+restaurants = []
+brand_names = ['макдоналдс', 'kfc', 'бургер кинг']
+getData(brand_names[0])
+getData(brand_names[1])
+getData(brand_names[2])
 print(constructMapURL(restaurants))
